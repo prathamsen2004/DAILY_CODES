1fe66d0430de98ba52538877bc30739b30d9cf5b
pratham <158561590+prathamsen2004@users.noreply.github.com>
2026-01-22 11:46:25 +0530

    Add backtracking permutation code

---
+++ backtracking.java
@@ -0,0 +1,18 @@
+public class backtracking      {
+    public static void printpermutation(String str,String perm,int idx){
+
+        if(str.length()==0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newstr = str.substring(0,i) + str.substring(i+1);
+            printpermutation(newstr,perm+currChar,idx+1);
+        }
+    }
+    public static void main(String args []){
+        String str = "ABC";
+        printpermutation(str, "" ,0);
+    }
+}
